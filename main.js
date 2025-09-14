a22e1bf52f53ece69be5a64c7648e3c02de20625
Srinu Vakada <164818954+srinu0906@users.noreply.github.com>
2025-09-14 18:39:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-const isPortrait = window.matchMedia("(orientation:portrait)").matches;
-if(isPortrait)
-{
-  alert("Please view this page in landscape orientation for optimal experience ");
+function toggleNav() {
+    const symbol = document.getElementById('toggle-nav').innerText;
+    document.getElementById("toggle-nav").innerText = (symbol == "✖") ? "☰" : "✖";
+
+    const toggle = document.getElementById('toggle-nav');
+    if (toggle.classList.contains('cross')) {
+        toggle.classList.remove('cross');
+        toggle.classList.add("bar")
+    } else {
+        toggle.classList.remove("bar");
+        toggle.classList.add("cross");
+    }
+
+    const navBar = document.getElementById("nav-bar");
+
+    if (toggle.classList.contains("bar")) {
+
+        navBar.classList.remove("show");
+        navBar.classList.add("hide");
+
+        navBar.addEventListener("animationend", () => {
+            if (toggle.classList.contains("bar"))
+                navBar.style.display = "none";
+        })
+
+    } else {
+        navBar.style.display = "flex";
+        navBar.classList.remove("hide");
+        navBar.classList.add("show");
+    }
 }
